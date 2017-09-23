ac154b2afdf4eaae7a9146e2ab4cf105ed42d166
M Gosschalk <monica.gosschalk@btinternet.com>
2017-09-23 19:10:48 +0100

    added material-ui

--- src/components/PhotoGrid.js
+++ src/components/PhotoGrid.js
@@ -2,37 +2,48 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { addComment, removeComment, increment } from '../actions';
 import './PhotoGrid.css';
-import Photo from './photo';
-import { Grid, Row, Col } from 'react-flexbox-grid';
-// https://www.codementor.io/christiannwamba/build-a-react-image-gallery-with-cloudinary-xh1cekno3
-// http://joelhooks.com/blog/2016/03/20/build-an-image-gallery-using-redux-saga
+//import Photo from './photo';
+import { GridList, GridTile } from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import Subheader from 'material-ui/Subheader';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+// material ui http://www.material-ui.com/#/components/grid-list
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+    overflowY: 'auto',
+  },
+};
+
 class PhotoGrid extends Component {
-
-
   render() {
     return (
-      <div className="container">
-        <ul className="list-group">
-            {/*
-              this.props.posts.map( (post, i) => <Photo {...this.props} key={i} i={i} post={post}/> )
-            */}
-            {
-              this.props.posts.map( (post, i) => {
-                return (
-                  <li className="list-group-item" key={i} flex-row align-items-start>
-                      <img className="image" src={post.display_src} alt={post.caption} />
-                      <div>{post.caption}</div>
-                  </li>
-                )
-              })
-            }
-        </ul>
-      </div>
+      <div style={styles.root}>
+        <GridList cellHeight={180} styles={styles.gridList}>
+          {/*
+            this.props.posts.map( (post, i) => <Photo {...this.props} key={i} i={i} post={post}/> )
+          */}
+          {
+            this.props.posts.map( (post, i) => {
+              return (
+                <GridTile key={i} title={post.caption}>
+                  <img src={post.display_src}/>
+                </GridTile>
+              )
+            })
+          }
+        </GridList>
     );
   }
 }
 
-//PhotoGrid = connect(PhotoGrid)
 const mapStateToProps = (state) => {
   return {
     posts: state.posts,
