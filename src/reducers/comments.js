c2c3752bb68ce46950daeaf146af432bd777abd1
M Gosschalk <monica.gosschalk@btinternet.com>
2017-09-24 15:03:24 +0100

    worked on reducers

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -2,19 +2,16 @@ import comments_json from '../data/comments';
 import { REMOVE_COMMENT, ADD_COMMENT } from '../actions';
 
 
+
 function comments(state = comments_json, action) {
   switch (action.type) {
     case ADD_COMMENT:
-    const postId = action.postId;
+      const postId = action.postId;
       return [
         ...state.push(action.postId, action.author, action.comment)
       ]
     case REMOVE_COMMENT:
-      const i = action.i;
-      return [
-        ...state.slice(0, i ),
-        ...state.slice(i, + 1 )
-      ]
+      return state;
     default:
       console.log('comments state ', state);
       return state;
